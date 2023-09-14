db29cebbff91850cad9c5468020acdaa549d59cc
Alan Vitório <alanvitorio1985@gmail.com>
2023-09-14 20:56:57 -0300

    trocando section de lugar, teste mobile

--- app/components/Finalizar.js
+++ app/components/Finalizar.js
@@ -1,19 +1,25 @@
 import React from 'react';
-import Image from 'next/image'; // Importar o componente Image de next/image
+import Image from 'next/image';
 
 export default function Finalizar() {
   return (
-    <div className={styles.centeredContainer}>
-      <Image
-        src="/caminho/para/a/imagem/do/palhaco.jpg" // Substitua pelo caminho correto
-        alt="Palhaço Fuinha"
-        width={300} // Defina a largura que você quer
-        height={300} // Defina a altura que você quer
-      />
-      <p className={styles.caption}>
-        Ajude o palhaço Fuinha, o palhaço mais sem graça do Brasil, a fazer um
-        feliz dia das crianças
-      </p>
-    </div>
+    <section className="bg-light">
+      <div className="container">
+        <div className="row justify-content-center align-items-center vh-100">
+          <div className="col text-center">
+            <Image
+              src="/caminho/para/a/imagem/do/palhaco.jpg"
+              alt="Palhaço Fuinha"
+              width={300}
+              height={300}
+            />
+            <p>
+              Ajude o palhaço Fuinha, o palhaço mais sem graça do Brasil, a
+              fazer um feliz dia das crianças
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 }
--- app/components/Sobre.js
+++ app/components/Sobre.js
@@ -1,22 +1,55 @@
 import React from 'react';
-import Image from 'next/image';
 
-export default function Finalizar() {
+import Image from 'next/image'; // Importar o componente Image de next/image
+
+export default function Sobre() {
   return (
-    <section className="">
-      <div className="container">
-        <div className="row justify-content-center align-items-center vh-100">
-          <div className="col text-center">
-            <Image
-              src="/caminho/para/a/imagem/do/palhaco.jpg"
-              alt="Palhaço Fuinha"
-              width={300}
-              height={300}
-            />
-            <p>
-              Ajude o palhaço Fuinha, o palhaço mais sem graça do Brasil, a
-              fazer um feliz dia das crianças
+    <section id="Sobre" className="bg-color-meu">
+      <div className="container pt-3">
+        <div className="text-center">
+          <h2 className="">A comunidade do Caqui</h2>
+          <div className="blue-divider "></div>{' '}
+        </div>
+        <div className="row">
+          {/* Coluna para o texto */}
+          <div className="col-md-6">
+            <p className="pt-3">
+              O Projeto começou no ano de 2019, Renata e seus familiares,
+              queriam fazer a diferença. Pra isso escolheram a sua cidade Natal,
+              Diadema.
             </p>
+            <p>
+              A comunidade do Caqui fica localizada no Eldorado, Diadema.
+              Atualmente conta com quase 100 pessoas vivendo em situação de
+              vulnerabilidade social.
+            </p>
+            <p>
+              Muito dos moradores são crianças e adolescentes.Muitas famílias
+              antes do assentamento, viviam literalmente dormindo na rua,
+              situação que se intensificou com a pandemia.
+            </p>
+            <p>
+              Nesse ano de 2023, a prefeitura tentou uma reintegração de posse
+              forçada, deixando as famílias que já eram vulneráveis em uma
+              situação ainda pior.
+            </p>
+            <p>
+              Diante de um cenário tão desolador, Renata e seus familiares este
+              ano estão novamente se mobilizando para ajudar a comunidade do
+              Caqui. Ajudar as crianças a terem dia das crianças mais feliz,
+              trazer alegria para as crianças em um cenário tão difícil.
+            </p>
+          </div>
+
+          {/* Coluna para a foto */}
+          <div className="col-md-6 pt-3">
+            <Image
+              src="/chacara.jpg"
+              alt="Foto da comunidade"
+              width={900} // Defina as dimensões que preferir
+              height={100}
+              className="img-fluid"
+            />
           </div>
         </div>
       </div>
