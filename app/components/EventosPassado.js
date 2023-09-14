cea47c9cecc1df756dee33f07700143e9283e8a1
Alan Vitório <alanvitorio1985@gmail.com>
2023-09-14 19:56:15 -0300

    arrumando bootstrap

--- app/components/EventosPassado.js
+++ app/components/EventosPassado.js
@@ -1,6 +1,6 @@
 'use client';
+import React, { useState } from 'react';
 
-import React from 'react';
 import Image from 'next/image';
 
 const imagensPorAno = {
@@ -30,73 +30,98 @@ export default function EventosPassados() {
       <div className="container pt-5">
         <div className="text-center mb-5">
           <h2 className="text-center">Projeto Família Amor Eterno</h2>
-          <div className="blue-divider "></div> {/* Centralizado */}
+          <div className="blue-divider"></div>
         </div>
-        <p>O projeto conta com diversas pessoas que se mobilizaram...</p>
-
         <div className="row">
-          <div className="col-2"></div> {/* Espaço vazio à esquerda */}
+          <div className="col-md-6 d-flex justify-content-center align-items-center">
+            <Image
+              src="/foto1-20.webp"
+              alt="Background Image"
+              width={500}
+              height={300}
+              layout="responsive"
+            />
+          </div>
+          <div className="col-md-6">
+            <p>
+              O projeto conta com diversas pessoas que se mobilizaram para
+              ajudar as familias carentes de nossa cidade.
+            </p>
+            <p>
+              A cada ano o projeto vai recebendo mais ajuda e mais pessoas se
+              conscientizando da importância de ajudar o próximo.
+            </p>
+            <p>
+              A campanha de 2020 foi particularmente difícil, pois o mundo todo
+              estava passando por uma pandemia e muitas pessoas perderam seus
+              empregos e não tinham como sustentar suas famílias. Mesmo assim,
+              conseguimos coletar até brinquedo para as crianças.
+            </p>
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-2"></div>
           <div className="col-8">
             {Object.keys(imagensPorAno).map((ano) => (
               <div key={ano}>
                 <h2 className="text-center mb-2 mt-3 ">
                   Campanha de doação de {ano}
                 </h2>
-                <div className="blue-divider "></div>{' '}
-                <div
-                  id={`carousel${ano}`}
-                  className="carousel slide "
-                  data-bs-ride="carousel"
-                >
-                  <div className="carousel-inner">
-                    {imagensPorAno[ano].map((imagem, index) => (
-                      <div
-                        className={`carousel-item ${
-                          index === 0 ? 'active' : ''
-                        }`}
-                        key={index}
-                      >
-                        <Image
-                          src={imagem}
-                          alt={`Projeto ${ano}`}
-                          width={730}
-                          height={550}
-                          layout="responsive"
-                        />
-                      </div>
-                    ))}
-                  </div>
-                  <button
-                    className="carousel-control-prev"
-                    type="button"
-                    data-bs-target={`#carousel${ano}`}
-                    data-bs-slide="prev"
-                  >
-                    <span
-                      className="carousel-control-prev-icon"
-                      aria-hidden="true"
-                    ></span>
-                    <span className="visually-hidden">Previous</span>
-                  </button>
-                  <button
-                    className="carousel-control-next"
-                    type="button"
-                    data-bs-target={`#carousel${ano}`}
-                    data-bs-slide="next"
-                  >
-                    <span
-                      className="carousel-control-next-icon"
-                      aria-hidden="true"
-                    ></span>
-                    <span className="visually-hidden">Next</span>
-                  </button>
-                </div>
+                <div className="blue-divider"></div>
+                <CustomCarousel images={imagensPorAno[ano]} />
               </div>
             ))}
           </div>
-          <div className="col-2"></div> {/* Espaço vazio à direita */}
+          <div className="col-2"></div>
         </div>
       </div>
     </section>
+  );
+}
+
+function CustomCarousel({ images }) {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const goToPrevSlide = () => {
+    let newIndex = activeIndex - 1;
+    if (newIndex < 0) {
+      newIndex = images.length - 1;
+    }
+    setActiveIndex(newIndex);
+  };
+
+  const goToNextSlide = () => {
+    let newIndex = activeIndex + 1;
+    if (newIndex >= images.length) {
+      newIndex = 0;
+    }
+    setActiveIndex(newIndex);
+  };
+
+  return (
+    <div style={{ position: 'relative' }}>
+      {images.map((image, index) => (
+        <div
+          className={`carousel-image ${
+            index === activeIndex ? 'd-block' : 'd-none'
+          }`}
+          key={index}
+        >
+          <Image
+            src={image}
+            alt={`Projeto ${index}`}
+            width={730}
+            height={550}
+            layout="responsive"
+          />
+        </div>
+      ))}
+      <button onClick={goToPrevSlide} className="carousel-button prev">
+        &#8249;
+      </button>
+      <button onClick={goToNextSlide} className="carousel-button next">
+        &#8250;
+      </button>
+    </div>
   );
 }
